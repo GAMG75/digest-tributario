d1c672d1f815bbc72913d1c3c4a2f611e73b828b
GAMG75 <gerardo.montes@gmail.com>
2026-05-18 09:13:59 -0400

    Update sii_scraper.py

--- scrapers/sii_scraper.py
+++ scrapers/sii_scraper.py
@@ -32,9 +32,9 @@ MESES = {
 }
 
 URLS_SII = {
-    "circulares": f"{BASE_URL}/normativa_legislacion/circulares.htm",
-    "resoluciones": f"{BASE_URL}/normativa_legislacion/resoluciones.htm",
-    "jurisprudencia": f"{BASE_URL}/normativa_legislacion/jurisprudencia_administrativa/index.html",
+    "circulares": f"{BASE_URL}/normativa_legislacion/circulares/2025/indcir2025.htm",
+    "resoluciones": f"{BASE_URL}/normativa_legislacion/resoluciones/2025/res_ind2025.htm",
+    "jurisprudencia": f"{BASE_URL}/normativa_legislacion/jurisprudencia_administrativa/2025/index.html",
 }
 
 
